019f61297c1aa10af403f59d79d341c86ed5e7e4
Kevga <9109613+Kevga@users.noreply.github.com>
2023-05-17 18:51:24 +0200

    Add BE2 project for ResourceRedirector

---
+++ src/XUnity.ResourceRedirector.BepInEx-IL2CPP-BE2/ResourceRedirectorPlugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using XUnity.ResourceRedirector.Constants;
+
+namespace XUnity.ResourceRedirector.BepInEx
+{
+   [BepInPlugin( PluginData.Identifier, PluginData.Name, PluginData.Version )]
+   public class ResourceRedirectorPlugin : BasePlugin
+   {
+      public static ConfigEntry<bool> LogAllLoadedResources { get; set; }
+      public static ConfigEntry<bool> LogCallbackOrder { get; set; }
+
+      public override void Load()
+      {
+         LogAllLoadedResources = Config.Bind( new ConfigDefinition( "Diagnostics", "Log all loaded resources" ), false );
+         ResourceRedirection.LogAllLoadedResources = LogAllLoadedResources.Value;
+         LogAllLoadedResources.SettingChanged += ( s, e ) => ResourceRedirection.LogAllLoadedResources = LogAllLoadedResources.Value;
+
+         LogCallbackOrder = Config.Bind( new ConfigDefinition( "Diagnostics", "Log callback order" ), false );
+         ResourceRedirection.LogCallbackOrder = LogCallbackOrder.Value;
+         LogCallbackOrder.SettingChanged += ( s, e ) => ResourceRedirection.LogCallbackOrder = LogCallbackOrder.Value;
+
+         Config.ConfigReloaded += Config_ConfigReloaded;
+      }
+
+      private static void Config_ConfigReloaded( object sender, EventArgs e )
+      {
+         ResourceRedirection.LogAllLoadedResources = LogAllLoadedResources.Value;
+         ResourceRedirection.LogCallbackOrder = LogCallbackOrder.Value;
+      }
+   }
+}
